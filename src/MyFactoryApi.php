6d62597d168b1c0f8b0d44e93d4d42f90341be5a
Florian Ratz <florian.ratz@supsign.ch>
2020-10-07 13:57:56 +0200

    no message

--- src/MyFactoryApi.php
+++ src/MyFactoryApi.php
@@ -6,4 +6,36 @@ use Config;
 
 class MyFactoryApi
 {
+    protected
+        $client = null,
+        $request = array(),
+        $response = null;
+
+	public function __construct() 
+	{
+
+	}
+
+	protected function clearRequestData() 
+	{
+		foreach ($this->request AS $key => $value) {
+			unset($this->request[$key]);
+		}
+
+		return $this;
+	}
+
+    public function getResponse() 
+    {
+        return $this->response;
+    }
+
+    protected function setRequestData(array $data)
+    {
+    	$this
+    		->clearRequestData()
+    		->request = array_merge($this->request, $data);
+
+    	return $this;
+    }
 }
