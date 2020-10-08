b163373f3cb3f2aaf57402f0d461f7134485f1b4
Florian Ratz <florian.ratz@supsign.ch>
2020-10-08 09:14:08 +0200

    WIP

--- src/MyFactoryApi.php
+++ src/MyFactoryApi.php
@@ -7,9 +7,14 @@ use Config;
 class MyFactoryApi
 {
     protected
+    	$ch = null,
         $client = null,
+        $endpoint = '',
+        $login = 'supsignHub',
+        $password = 'kH5bI7sT3oJ3iN0k',
         $request = array(),
-        $response = null;
+        $response = null,
+        $url = 'https://cloud.myfactory-ondemand.ch/saas/odata_howecekavikubisojoqa33/';
 
 	public function __construct() 
 	{
@@ -25,10 +30,62 @@ class MyFactoryApi
 		return $this;
 	}
 
+	protected function createRequest() 
+	{
+		$this->ch = curl_init();
+
+		curl_setopt($this->ch, CURLOPT_URL, $this->url.$this->endpoint);
+		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+		return $this;
+	}
+
+	public function getEndPoints()
+	{
+		$this->endpoint = '';
+
+		$workspace = $this->getResponse()->workspace;
+
+		var_dump(
+			$workspace->collection
+		);
+
+
+
+
+		// $results = $this->getResponse()->response->workspace->collection;
+
+		// foreach ($results AS $result) {
+		// 	var_dump(
+		// 		$result->{'@attributes'}->href
+		// 	);
+		// }
+
+
+
+	}
+
     public function getResponse() 
     {
+    	if (!$this->response) {
+    		$this->sendRequest();
+    	}
+
         return $this->response;
     }
+
+	protected function sendRequest() 
+	{
+		if (!$this->ch) {
+			$this->createRequest();
+		}
+
+		$this->response = simplexml_load_string(curl_exec($this->ch) );
+		curl_close($this->ch);
+
+		return $this;
+	}
 
     protected function setRequestData(array $data)
     {
@@ -38,4 +95,11 @@ class MyFactoryApi
 
     	return $this;
     }
+
+	public function test() 
+	{
+		$this->getEndPoints();
+
+		return $this;
+	}
 }
