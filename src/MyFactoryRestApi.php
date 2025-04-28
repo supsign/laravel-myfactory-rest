3246d8bd0de89b9b5ff0627c87a5197cfd18b139
Florian Ratz <florian.ratz@supsign.ch>
2025-04-28 10:30:40 +0200

    no message

--- src/MyFactoryRestApi.php
+++ src/MyFactoryRestApi.php
@@ -2,457 +2,113 @@
 
 namespace Supsign\LaravelMfRest;
 
-use Exception;
-use stdClass;
+use Illuminate\Support\Collection;
+use Supsign\Laravel\BaseApi;
 
-class MyFactoryRestApi
+class MyFactoryRestApi extends BaseApi
 {
-    protected
-    	$cache = array(),
-    	$ch = null,
-        $endpoint = '',
-        $endpoints = array(),
-        $login = null,
-        $parameters = array(),
-        $password = null,
-        $request = array(),
-        $response = null,
-        $responseRaw = array(),
-        $skipStep = 5000,
-        $url = null;
+	protected array $headers = [
+		'Accept' => 'application/json'
+	];
 
 	public function __construct() 
 	{
-		$this->login = env('MF_REST_LOGIN');
-		$this->password = env('MF_REST_PASSWORD');
-		$this->url = env('MF_REST_URL');
+		$this->clientId = env('MF_REST_LOGIN');
+		$this->clientSecret = env('MF_REST_PASSWORD');
+		$this->baseUrl = env('MF_REST_URL');
 
-		return $this;
+		return $this->useBasicAuth();
 	}
 
-	protected function clearCache()
+	public function getAddresses(): array
 	{
-		$this->cache = array();
-
-		return $this;
+		return $this->makeCall('Adressen');
 	}
 
-	public function clearResponse()
+	public function getAddressGroups(): array
 	{
-		$this->response = null;
-		$this->responseRaw = array();
-		$this->parameters = array();
-
-		return $this;
+		return $this->makeCall('Adressgruppen');
 	}
 
-	protected function clearRequestData() 
+	public function getCustomers(): array
 	{
-		foreach ($this->request AS $key => $value) {
-			unset($this->request[$key]);
-		}
-
-		return $this;
+		return $this->makeCall('Kunden');
 	}
 
-	protected function createRequest() 
+	public function getCustomerGroups(): array
 	{
-		$this->ch = curl_init();
-
-		if ($this->endpoint) {
-			curl_setopt($this->ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-			curl_setopt($this->ch, CURLOPT_USERPWD, $this->login.':'.$this->password);
-		}
-
-		curl_setopt($this->ch, CURLOPT_URL, $this->url.$this->endpoint.$this->getParamterString());
-		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, 'GET');
-
-		return $this;
+		return $this->makeCall('Kundengruppen');
 	}
 
-	public function getAddress(int|string $id)
-	{
-		foreach ($this->getAddresses() AS $address) {
-			if ($address->PK_KundeID === $id OR $address->Kundennummer === $id) {
-				return $address;
-			}
-		}
-
-		return null;
-	}
-
-	public function getAddresses()
-	{
-		if (isset($this->cache['addresses'])) {
-			return $this->cache['addresses'];
-		}
-
-		$this->endpoint = 'Adressen';
-
-		return $this->cache['addresses'] = $this->clearResponse()->getResponse();
-	}
-
-	public function getAddressGroups()
-	{
-		if (isset($this->cache['addressGroups'])) {
-			return $this->cache['addressGroups'];
-		}
-
-		$this->endpoint = 'Adressgruppen';
-
-		return $this->cache['addressGroups'] = $this->clearResponse()->getResponse();
-	}
-
-	public function getCustomer(int|string $id): ?stdClass
-	{
-		foreach ($this->getCustomers() AS $customer) {
-			if ((int)$customer->PK_KundeID === (int)$id OR $customer->Kundennummer === $id) {
-				return $customer;
-			}
-		}
-
-		return null;
-	}
-
-	public function getCustomers()
-	{
-		if (isset($this->cache['customers'])) {
-			return $this->cache['customers'];
-		}
-
-		$this->endpoint = 'Kunden';
-
-		return $this->cache['customers'] = $this->clearResponse()->getResponse();
-	}
-
-	public function getCustomerGroups()
-	{
-		if (isset($this->cache['customerGroups'])) {
-			return $this->cache['customerGroups'];
-		}
-
-		$this->endpoint = 'Kundengruppen';
-
-		return $this->cache['customerGroups'] = $this->clearResponse()->getResponse();
-	}
-
-	public function getEndpoint() {
-		return $this->endpoint;
-	}
-
-	public function getProduct($id)
-	{
-		foreach ($this->getProducts() AS $product) {
-			if ($product->PK_ArtikelID == $id OR $product->Artikelnummer == $id) {
-				return $product;
-			}
-		}
-
-		return null;
-	}
-
-	public function getExpenses()
-	{
-		if (isset($this->cache['expenses'])) {
-			return $this->cache['expenses'];
-		}
-
-		$this->endpoint = 'ZeiteintraegeSpesen';
-
-		return $this->cache['expenses'] = $this->clearResponse()->getResponse();
-	}
-
-	public function getProducts()
+	public function getProducts(): array
 	{		
-		if (isset($this->cache['products'])) {
-			return $this->cache['products'];
-		}
-
-		$this->endpoint = 'Artikel';
-		return $this->cache['products'] = $this->clearResponse()->getResponse();
+		return $this->makeCall('Artikel');
 	}
 
-	public function getProductGroup($id)
+	public function getProductGroups(): array
 	{
-		foreach ($this->getProductGroups() AS $productGroup) {
-			if ($productGroup->PK_ArtikelgruppeKurz == $id) {
-				return $productGroup;
-			}
-		}
-
-		throw new \Exception('productGroup not found', 1);
+		return $this->makeCall('Artikelgruppen');
 	}
 
-	public function getProductGroups()
+	public function getProjects(): array
 	{
-		if (isset($this->cache['productsGroups'])) {
-			return $this->cache['productsGroups'];
-		}
-
-		$this->endpoint = 'Artikelgruppen';
-		return $this->cache['productsGroups'] = $this->clearResponse()->getResponse();
+		return $this->makeCall('Projekte');
 	}
 
-	public function getProjects()
+	public function getProjectGroups(): array
 	{
-		if (isset($this->cache['projects'])) {
-			return $this->cache['projects'];
-		}
-
-		$this->endpoint = 'Projekte';
-
-		return $this->cache['projects'] = $this->clearResponse()->getResponse();
+		return $this->makeCall('Projektgruppen');
 	}
 
-	public function getProjectGroups()
+	public function getRessources(): array
 	{
-		if (isset($this->cache['projectGroups'])) {
-			return $this->cache['projectGroups'];
-		}
-
-		$this->endpoint = 'Projektgruppen';
-
-		return $this->cache['projectGroups'] = $this->clearResponse()->getResponse();
+		return $this->makeCall('Ressource');
 	}
 
-	public function getRessources()
+	public function getSalesOrders(): array
 	{
-		if (isset($this->cache['ressources'])) {
-			return $this->cache['ressources'];
-		}
-
-		$this->endpoint = 'Ressource';
-
-		return $this->cache['ressources'] = $this->clearResponse()->getResponse();
+		return $this->makeCall('Verkaufsbelege');
 	}
 
-	public function getSalesOrder($id)
+	public function getSalesOrderPositions(?int $salesOrderId = null): array
 	{
-		foreach ($this->getSalesOrders() AS $salesOrder) {
-			if ($salesOrder->PK_BelegID == $id) {
-				return $salesOrder;
-			}
+		if (!is_null($salesOrderId)) {
+			$this->useCache();
 		}
 
-		return null;
+		return collect($this->makeCall('VerkaufsbelegPositionen'))
+			->when(
+				!is_null($salesOrderId),
+				fn (Collection $collection): Collection => $collection->filter(
+					fn (object $position): bool => $position->FK_BelegID === $salesOrderId,
+				),
+			)
+			->values()
+			->toArray();
 	}
 
-	public function getSalesOrders()
+	public function getSupportTickets(): array
 	{
-		if (isset($this->cache['salesOrders'])) {
-			return $this->cache['salesOrders'];
-		}
-
-		$this->endpoint = 'Verkaufsbelege';
-
-		return $this->cache['salesOrders'] = $this->clearResponse()->getResponse();
+		return $this->makeCall('Supportfaelle');
 	}
 
-	public function getSalesOrderPosition($id)
+	public function getTargetWorkingTime(): array
 	{
-		foreach ($this->getSalesOrderPositions() AS $salesOrderPosition) {
-			if ($salesOrderPosition->PK_BelegPosID == $id) {
-				return $salesOrderPosition;
-			}
-		}
-
-		throw new \Exception('position not found', 1);
+		return $this->makeCall('idcMitarbeiterSollArbeitszeiten');
 	}
 
-	public function getSalesOrderPositions($id = null)
+	public function getTimeEntries(): array
 	{
-		if (isset($this->cache['salesOrderPositions'])) {
-			if ($id) {
-				foreach ($this->cache['salesOrderPositions'] AS $entry) {
-					if ($entry->FK_BelegID == $id) {
-						$result[] = $entry;
-					}
-				}
-
-				return $result ?? [];
-			}
-
-			return $this->cache['salesOrderPositions'];
-		}
-
-		$this->endpoint = 'VerkaufsbelegPositionen';
-		$this->cache['salesOrderPositions'] = $this->clearResponse()->getResponse();
-
-		return $this->getSalesOrderPositions($id);
-	}
-
-	public function getSupportTickets()
-	{
-		if (isset($this->cache['supperTickets'])) {
-			return $this->cache['supperTickets'];
-		}
-
-		$this->endpoint = 'Supportfaelle';
-
-		return $this->cache['supperTickets'] = $this->clearResponse()->getResponse();
-	}
-
-	public function getTargetWorkingTime()
-	{
-		if (isset($this->cache['targetWorkingTime'])) {
-			return $this->cache['targetWorkingTime'];
-		}
-
-		$this->endpoint = 'idcMitarbeiterSollArbeitszeiten';
-
-		return $this->cache['targetWorkingTime'] = $this->clearResponse()->getResponse();	
-	}
-
-	public function getTimeEntries()
-	{
-		if (isset($this->cache['timeEntries'])) {
-			return $this->cache['timeEntries'];
-		}
-
-		$this->endpoint = 'Zeiteintraege';
-
-		return $this->cache['timeEntries'] = $this->clearResponse()->getResponse();
+		return $this->makeCall('Zeiteintraege');
 	}
 
 	public function getUsers()
 	{
-		if (isset($this->cache['users'])) {
-			return $this->cache['users'];
-		}
-
-		$this->endpoint = 'Benutzer';
-
-		return $this->cache['users'] = $this->clearResponse()->getResponse();
+		return $this->makeCall('Benutzer');
 	}
 
-	protected function getParamterString()
-	{
-		if (!$this->parameters) {
-			return '';
-		}
-
-		foreach ($this->parameters AS $key => $value) {
-			$pairs[] = implode('=', [$key, $value]);
-		}
-
-		return '?'.implode('&', $pairs);
-	}
-
-	protected static function getProperties($element) 
-	{
-		return $element->content->children('m', true)->properties->children('d', true);
-	}
-
-    public function getResponse() 
+    protected function getResponse(): array|object
     {
-    	if (!$this->endpoint) {
-    		throw new Exception('no endpoint specified', 1);
-    	}
-
-    	if (!$this->response) {
-    		$this->sendRequests();
-    	}
-
-    	return $this->response;
-    }
-
-	protected function sendRequest()
-	{
-		$this->createRequest();
-		$this->setResponse(simplexml_load_string(curl_exec($this->ch)));
-		curl_close($this->ch);
-
-		return $this;
-	}
-
-    protected function sendRequests()
-    {
-    	do {
-    		$this->sendRequest();
-
-			if (!isset($this->parameters['$skip'])) {
-				$this->parameters['$skip'] = $this->skipStep;
-			} else {
-				$this->parameters['$skip'] += $this->skipStep;
-			}
-    	} while (!$this->requestFinished);
-
-    	$this->response = self::toStdClass($this->responseRaw);
-
-    	return $this;
-    }
-
-	public function setEndpoint($endpoint) {
-		$this->endpoint = $endpoint;
-
-		return $this;
-	}
-
-    protected function setRequestData(array $data)
-    {
-    	$this
-    		->clearRequestData()
-    		->request = array_merge($this->request, $data);
-
-    	return $this;
-    }
-
-    protected function setResponse($response) 
-    {
-    	if (isset($response->workspace)) {
-    		if (isset($response->workspace->collection)) {
-    			$this->response = $response->workspace->collection;
-    		} else {
-    			$this->response = $response->workspace;
-    		}
-
-    		return $this;
-    	}
-
-    	if (!isset($response->entry)) {
-    		dd(
-    			$this->url,
-    			$this->endpoint,
-    			$this->getParamterString(), 
-    			$response
-    		);
-
-    		throw new Exception('not entry element found', 1);
-    	}
-
-    	$data = array();
-
-    	foreach ($response->entry AS $entry) {
-    		$data[] = self::getProperties($entry);
-    	}
-
-    	$this->requestFinished = count($data) % $this->skipStep !== 0;
-    	$this->responseRaw = array_merge($this->responseRaw, $data);
-
-		return $this;
-    }
-
-    protected static function toStdClass($collection) 
-    {
-    	$collection = json_decode(json_encode($collection));
-
-    	foreach ($collection AS $entry) {
-    		foreach ($entry AS $key => $value) {
-    			if (is_object($value)) {
-    				$entry->$key = null;
-    			}
-
-    			switch ($key) {
-    				case 'EANNummer':
-    					if (!is_numeric($value) OR $value == 0 OR floor(log10($value) + 1) != 13) {
-    						$entry->$key = null;
-    					}
-    					break;	
-    			}
-    		}
-    	}
-
-    	return $collection;
+        return $this->response->value;
     }
 }
